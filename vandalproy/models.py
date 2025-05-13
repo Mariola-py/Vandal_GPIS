61d8d458dbb774d5b3828ca9e2f992c31265927f
TofikVP <tofik.varo@goumh.umh.es>
2025-05-13 20:05:08 +0200

    Update models.py

--- vandalproy/models.py
+++ vandalproy/models.py
@@ -2,23 +2,6 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.nombre
-
-class Pagina(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100, unique=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    imagen = models.ImageField(verbose_name='Imagen', null=True, blank=True)
-    fecha_creacion = models.DateTimeField(default=now, editable=False)
-    fecha_modificacion = models.DateTimeField(auto_now=True, editable=False)
-
-    def __str__(self):
-        return self.nombre
-
 class UserRole(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=[('redactor', 'Redactor'), ('colaborador', 'Colaborador'), ('suscriptor', 'Suscriptor')])
